fe8c67f413d6a646aca7840ab137fbcbb128edf8
H0308 <1848312235@qq.com>
2024-10-18 15:41:29 +0800

    二分查找算法及题目

--- Array(Basic)/BinarySearch/BinarySearch.cpp
+++ Array(Basic)/BinarySearch/BinarySearch.cpp
@@ -81,7 +81,7 @@ int Array_Basic::BinarySearch(std::vector<int>& nums, int target)
 }
 
 // 力扣35.搜索插入位置
-class Solution {
+class Solution35 {
 public:
     int searchInsert(std::vector<int>& nums, int target) {
         int left = 0;
@@ -111,3 +111,210 @@ public:
         return left;
     }
 };
+
+// 力扣69.x的平方根
+/*
+ * 二分查找：
+ * 为什么：在枚举小于x的每一个整数时，最小的算术平方根是0，依次增大，满足二分查找的调节：有序且严格递增
+ * 怎么用：最小的算术平方根是0，所以起点为0，而需要判断的是当前枚举的数值是否为k*k<=x，
+ * 而因为x的算术平方根肯定在[0, x]区间中，所以需要枚举0到x中的数值。
+ * 假设将0到x个数值看做一个数组，则取出当前数组的中间元素，
+ * 判断中间元素mid*mid是否<=x，小于说明需要移动left，否则移动right
+ * 怎么写：
+ */
+class Solution69 {
+public:
+    int mySqrt(int x) {
+        // 暴力解法
+        // size_t ans = 0;
+        // for(;ans * ans <= x; ans++);
+        // return (ans - 1);
+        size_t left = 0;
+        size_t right = x;
+        size_t ans = 0;
+        // 左闭右闭区间
+        while(left <= right)
+        {
+            size_t mid = left + ((right - left) >> 1);
+            // 可能无法刚刚好等于
+            // if(ans == x)
+            // {
+            //     ans = mid;
+            //     break;
+            // }
+            if(mid * mid <= x) // 中间值较小
+            {
+                ans = mid;
+                left = mid + 1;
+            }
+            else if(mid * mid > x) // 中间值较大
+            {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+// 类似69题型：力扣367.有效的完全平方数
+class Solution367 {
+public:
+    bool isPerfectSquare(int num) {
+        size_t left = 0;
+        size_t right = num;
+        size_t ans = 0;
+        // 左闭右闭区间
+        while(left <= right)
+        {
+            size_t mid = left + ((right - left) >> 1);
+            if (mid * mid <= num)
+            {
+                ans = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return ans * ans == num;
+    }
+};
+
+// 力扣34.在排序数组中查找元素的第一个和最后一个位置
+class Solution34 {
+public:
+    // 二分查找
+    int BinarySearch(std::vector<int>& nums, double target) {
+        int left = 0;
+        int right = nums.size();
+
+        // 左闭右开区间
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    // 使用二分查找确定是否存在
+    bool isExist(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+
+        // 左闭右开区间
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target){
+                right = mid;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    std::vector<int> searchRange(std::vector<int>& nums, int target) {
+        // 判断是否存在
+        if(!isExist(nums, target))
+        {
+            // 不存在直接返回
+            return {-1, -1};
+        }
+
+        // 存在则继续
+        // 找左边界
+        int leftHand = BinarySearch(nums, target - 0.5);
+        // 找右边界
+        int rightHand = BinarySearch(nums, target + 0.5);
+        return {leftHand, rightHand - 1};
+    }
+};
+
+// 牛客类似题目：JZ53 数字在升序数组中出现的次数
+#include <vector>
+class SolutionJZ53 {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums int整型vector
+     * @param k int整型
+     * @return int整型
+     */
+    // 二分查找
+    int BinarySearch(std::vector<int>& nums, double target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        // 左闭右闭区间
+        while (left <= right)
+        {
+            int mid = left + ((right - left) >> 1);
+            if(nums[mid] <= target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+    int GetNumberOfK(std::vector<int>& nums, int k)
+    {
+        // 左边界
+        int leftHand = BinarySearch(nums, k - 0.5);
+        // 右边界
+        int rightHand = BinarySearch(nums, k + 0.5);
+
+        return rightHand - leftHand;
+    }
+};
+// C语言版本
+namespace solutionJZ53
+{
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums int整型一维数组
+     * @param numsLen int nums数组长度
+     * @param k int整型
+     * @return int整型
+     */
+    //二分查找
+    int BinarySearch(int* nums, int numsLen, double k)
+    {
+        int left = 0;
+        int right = numsLen - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] > k)
+            {
+                right = mid - 1;
+            }
+            else if (nums[mid] < k) {
+                left = mid + 1;
+            }
+        }
+        //返回 left 而不是 mid
+        return left;
+    }
+    int GetNumberOfK(int* nums, int numsLen, int k )
+    {
+        return BinarySearch(nums, numsLen, k+0.5)-BinarySearch(nums, numsLen, k-0.5);
+    }
+}
