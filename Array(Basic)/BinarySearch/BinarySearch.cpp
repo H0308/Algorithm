b781be0ddea252c6319c2cc6ce34304140cbbd15
EPSDA <1848312235@qq.com>
2024-10-21 10:43:10 +0800

    移除元素相关题目+有序数据的平方+格式调整

--- Array(Basic)/BinarySearch/BinarySearch.cpp
+++ Array(Basic)/BinarySearch/BinarySearch.cpp
@@ -5,7 +5,7 @@
 int Array_Basic::BinarySearch(std::vector<int> &nums, int target, int flag)
 {
     // 使用(void)强制转换防止编译器警告，flag在二分查找中没有意义
-    (void)flag;
+    (void) flag;
     std::cout << "[left, right]" << std::endl;
     // 左闭右闭原则：[left, right]区间中所有值均在vector内有效
     int left = 0;
@@ -49,7 +49,7 @@ int Array_Basic::BinarySearch(std::vector<int> &nums, int target, int flag)
 
 // 左闭右开原则
 // 返回指定元素的下标
-int Array_Basic::BinarySearch(std::vector<int>& nums, int target)
+int Array_Basic::BinarySearch(std::vector<int> &nums, int target)
 {
     std::cout << "[left, right)" << std::endl;
     // 左闭右开原则：[left, right)，此时当left==right时就是无效区间
@@ -60,16 +60,16 @@ int Array_Basic::BinarySearch(std::vector<int>& nums, int target)
     while (left < right)
     {
         int mid = (left + right) / 2;
-        if(nums[mid] == target) // 相等——返回
+        if (nums[mid] == target) // 相等——返回
         {
             return mid;
         }
-        else if(nums[mid] > target) // 中间值较大，则目标值一定在中间值右边
+        else if (nums[mid] > target) // 中间值较大，则目标值一定在中间值右边
         {
             // 左闭右开原则，此时mid的值是下一次的右边界，不包括mid的值
             right = mid;
         }
-        else if(nums[mid] < target) // 中间值较小，则目标值一定在中间值左边
+        else if (nums[mid] < target) // 中间值较小，则目标值一定在中间值左边
         {
             // 左边右开原则，此时mid的值是下一次的左边界，包括mid的值
             left = mid + 1;
@@ -81,24 +81,26 @@ int Array_Basic::BinarySearch(std::vector<int>& nums, int target)
 }
 
 // 力扣35.搜索插入位置
-class Solution35 {
+class Solution35
+{
 public:
-    int searchInsert(std::vector<int>& nums, int target) {
+    int searchInsert(std::vector<int> &nums, int target)
+    {
         int left = 0;
         int right = nums.size() - 1;
         int mid = 0;
-        while(left <= right)
+        while (left <= right)
         {
             mid = left + ((right - left) >> 1);
-            if(nums[mid] == target)
+            if (nums[mid] == target)
             {
                 return mid;
             }
-            else if(nums[mid] > target)
+            else if (nums[mid] > target)
             {
                 right = mid - 1;
             }
-            else if(nums[mid] < target)
+            else if (nums[mid] < target)
             {
                 left = mid + 1;
             }
@@ -122,9 +124,11 @@ public:
  * 判断中间元素mid*mid是否<=x，小于说明需要移动left，否则移动right
  * 怎么写：
  */
-class Solution69 {
+class Solution69
+{
 public:
-    int mySqrt(int x) {
+    int mySqrt(int x)
+    {
         // 暴力解法
         // size_t ans = 0;
         // for(;ans * ans <= x; ans++);
@@ -133,7 +137,7 @@ public:
         size_t right = x;
         size_t ans = 0;
         // 左闭右闭区间
-        while(left <= right)
+        while (left <= right)
         {
             size_t mid = left + ((right - left) >> 1);
             // 可能无法刚刚好等于
@@ -142,12 +146,12 @@ public:
             //     ans = mid;
             //     break;
             // }
-            if(mid * mid <= x) // 中间值较小
+            if (mid * mid <= x) // 中间值较小
             {
                 ans = mid;
                 left = mid + 1;
             }
-            else if(mid * mid > x) // 中间值较大
+            else if (mid * mid > x) // 中间值较大
             {
                 right = mid - 1;
             }
@@ -157,14 +161,16 @@ public:
 };
 
 // 类似69题型：力扣367.有效的完全平方数
-class Solution367 {
+class Solution367
+{
 public:
-    bool isPerfectSquare(int num) {
+    bool isPerfectSquare(int num)
+    {
         size_t left = 0;
         size_t right = num;
         size_t ans = 0;
         // 左闭右闭区间
-        while(left <= right)
+        while (left <= right)
         {
             size_t mid = left + ((right - left) >> 1);
             if (mid * mid <= num)
@@ -191,19 +197,25 @@ public:
  * 怎么找：首先判断target是否存在于原数组，不存在返回{-1, -1}，此处就可以直接使用二分查找，至少找到一个有效位置即可，
  * 如果存在则继续使用两次二分查找，第一次二分查找找左边界，第二次找右边界即可
  */
-class Solution34 {
+class Solution34
+{
 public:
     // 二分查找
-    int BinarySearch(std::vector<int>& nums, double target) {
+    int BinarySearch(std::vector<int> &nums, double target)
+    {
         int left = 0;
         int right = nums.size();
 
         // 左闭右开区间
-        while (left < right) {
+        while (left < right)
+        {
             int mid = left + ((right - left) >> 1);
-            if (nums[mid] <= target) {
+            if (nums[mid] <= target)
+            {
                 left = mid + 1;
-            } else {
+            }
+            else
+            {
                 right = mid;
             }
         }
@@ -212,18 +224,25 @@ public:
     }
 
     // 使用二分查找确定是否存在
-    bool isExist(std::vector<int>& nums, int target) {
+    bool isExist(std::vector<int> &nums, int target)
+    {
         int left = 0;
         int right = nums.size();
 
         // 左闭右开区间
-        while (left < right) {
+        while (left < right)
+        {
             int mid = left + ((right - left) >> 1);
-            if (nums[mid] < target) {
+            if (nums[mid] < target)
+            {
                 left = mid + 1;
-            } else if (nums[mid] > target){
+            }
+            else if (nums[mid] > target)
+            {
                 right = mid;
-            } else {
+            }
+            else
+            {
                 return true;
             }
         }
@@ -231,9 +250,10 @@ public:
         return false;
     }
 
-    std::vector<int> searchRange(std::vector<int>& nums, int target) {
+    std::vector<int> searchRange(std::vector<int> &nums, int target)
+    {
         // 判断是否存在
-        if(!isExist(nums, target))
+        if (!isExist(nums, target))
         {
             // 不存在直接返回
             return {-1, -1};
@@ -250,7 +270,9 @@ public:
 
 // 牛客类似题目：JZ53 数字在升序数组中出现的次数
 #include <vector>
-class SolutionJZ53 {
+
+class SolutionJZ53
+{
 public:
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
@@ -261,7 +283,7 @@ public:
      * @return int整型
      */
     // 二分查找
-    int BinarySearch(std::vector<int>& nums, double target)
+    int BinarySearch(std::vector<int> &nums, double target)
     {
         int left = 0;
         int right = nums.size() - 1;
@@ -269,7 +291,7 @@ public:
         while (left <= right)
         {
             int mid = left + ((right - left) >> 1);
-            if(nums[mid] <= target)
+            if (nums[mid] <= target)
             {
                 left = mid + 1;
             }
@@ -281,7 +303,8 @@ public:
 
         return left;
     }
-    int GetNumberOfK(std::vector<int>& nums, int k)
+
+    int GetNumberOfK(std::vector<int> &nums, int k)
     {
         // 左边界
         int leftHand = BinarySearch(nums, k - 0.5);
@@ -291,6 +314,7 @@ public:
         return rightHand - leftHand;
     }
 };
+
 // C语言版本
 namespace solutionJZ53
 {
@@ -304,25 +328,28 @@ namespace solutionJZ53
      * @return int整型
      */
     //二分查找
-    int BinarySearch(int* nums, int numsLen, double k)
+    int BinarySearch(int *nums, int numsLen, double k)
     {
         int left = 0;
         int right = numsLen - 1;
-        while (left <= right) {
+        while (left <= right)
+        {
             int mid = (left + right) / 2;
-            if(nums[mid] > k)
+            if (nums[mid] > k)
             {
                 right = mid - 1;
             }
-            else if (nums[mid] < k) {
+            else if (nums[mid] < k)
+            {
                 left = mid + 1;
             }
         }
         //返回 left 而不是 mid
         return left;
     }
-    int GetNumberOfK(int* nums, int numsLen, int k )
+
+    int GetNumberOfK(int *nums, int numsLen, int k)
     {
-        return BinarySearch(nums, numsLen, k+0.5)-BinarySearch(nums, numsLen, k-0.5);
+        return BinarySearch(nums, numsLen, k + 0.5) - BinarySearch(nums, numsLen, k - 0.5);
     }
 }
--- Array(Basic)/RemoveElement/RemoveElement.cpp
+++ Array(Basic)/RemoveElement/RemoveElement.cpp
@@ -8,15 +8,17 @@
 // 力扣27.移除元素
 // 暴力解法，见网站
 // 双指针解法
-class Solution27 {
+class Solution27
+{
 public:
-    int removeElement(std::vector<int>& nums, int val) {
+    int removeElement(std::vector<int> &nums, int val)
+    {
         // 双指针解法
         // int count = 0;
         int fast = 0, slow = 0;
-        while(fast < nums.size())
+        while (fast < nums.size())
         {
-            if(nums[fast] != val)
+            if (nums[fast] != val)
             {
                 nums[slow++] = nums[fast];
             }
@@ -29,19 +31,21 @@ public:
 };
 
 // 力扣26.删除有序数组中的重复项
-class Solution26 {
+class Solution26
+{
 public:
-    int removeDuplicates(std::vector<int>& nums) {
+    int removeDuplicates(std::vector<int> &nums)
+    {
         // 双指针解法
         int size = nums.size();
         int slow = 0;
         int fast = 0;
-        while(fast < nums.size())
+        while (fast < nums.size())
         {
-            if(nums[fast] != nums[slow])
+            if (nums[fast] != nums[slow])
             {
                 // slow+1防止越界
-                if(slow + 1 < nums.size())
+                if (slow + 1 < nums.size())
                 {
                     nums[slow + 1] = nums[fast];
                 }
@@ -55,13 +59,15 @@ public:
 };
 
 // 力扣283.移动零
-class Solution283 {
+class Solution283
+{
 public:
-    void moveZeroes(std::vector<int>& nums) {
+    void moveZeroes(std::vector<int> &nums)
+    {
         int prev = 0, cur = 0;
-        while(cur < nums.size())
+        while (cur < nums.size())
         {
-            if(nums[cur])
+            if (nums[cur])
                 std::swap(nums[cur++], nums[prev++]);
             else
                 cur++;
@@ -71,19 +77,20 @@ public:
 
 // 力扣844.比较含退格的字符
 // 暴力解法——注意string类也有尾删操作
-class Solution844_1 {
+class Solution844_1
+{
 public:
     std::string remove(std::string s)
     {
         // 暴力解法
         std::string ret;
-        for(auto str : s)
+        for (auto str: s)
         {
-            if(str != '#')
+            if (str != '#')
             {
                 ret.push_back(str);
             }
-            else if(!ret.empty())
+            else if (!ret.empty())
             {
                 // string的尾删方法pop_back
                 ret.pop_back();
@@ -92,7 +99,9 @@ public:
 
         return ret;
     }
-    bool backspaceCompare(std::string s, std::string t) {
+
+    bool backspaceCompare(std::string s, std::string t)
+    {
         return remove(s) == remove(t);
     }
 };
@@ -107,25 +116,27 @@ public:
  * 2. 当有一方#计数器不为0，则让该方的下标指针向前移动，直到移动到计数器为0为止
  * 3. 如果当前字符是#，则#计数器加1，并且下标指针向前移动
  */
-class Solution {
+class Solution844_2
+{
 public:
-    bool backspaceCompare(std::string s, std::string t) {
+    bool backspaceCompare(std::string s, std::string t)
+    {
         int si = s.size() - 1, ti = t.size() - 1;
         int skips = 0, skipt = 0;
 
         // 确保至少有一方有内容
-        while(si >= 0 || ti >= 0)
+        while (si >= 0 || ti >= 0)
         {
             // 遍历第一个字符数组
-            while(si >= 0)
+            while (si >= 0)
             {
-                if(s[si] == '#')
+                if (s[si] == '#')
                 {
                     // 第三种情况
                     skips++;
                     si--;
                 }
-                else if(skips > 0)
+                else if (skips > 0)
                 {
                     // 第二种情况
                     si--;
@@ -140,13 +151,13 @@ public:
             // 遍历第二个字符数组
             while (ti >= 0)
             {
-                if(t[ti] == '#')
+                if (t[ti] == '#')
                 {
                     // 第三种情况
                     skipt++;
                     ti--;
                 }
-                else if(skipt > 0)
+                else if (skipt > 0)
                 {
                     // 第二种情况
                     ti--;
@@ -161,9 +172,9 @@ public:
 
             // 判断两个字符串当前字符是否相等
             // 必须确保两个字符串都有字符才进行当前字符是否相等的比较
-            if(si >= 0 && ti >= 0)
+            if (si >= 0 && ti >= 0)
             {
-                if(s[si] != t[ti])
+                if (s[si] != t[ti])
                 {
                     return false;
                 }
@@ -171,7 +182,7 @@ public:
             else
             {
                 // 如果有一个字符数组为空，则直接返回false
-                if(si >= 0 || ti >= 0)
+                if (si >= 0 || ti >= 0)
                 {
                     return false;
                 }
@@ -187,15 +198,83 @@ public:
 
 // 力扣977.有序数组的平方
 // 尾插+排序
-class Solution977_1 {
+class Solution977_1
+{
 public:
-    std::vector<int> sortedSquares(std::vector<int>& nums) {
+    std::vector<int> sortedSquares(std::vector<int> &nums)
+    {
         std::vector<int> ret;
-        for(auto num : nums)
+        for (auto num: nums)
         {
             ret.push_back(num * num);
         }
         std::sort(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
+// 双指针算法（但非原地处理）
+/*
+ * 为什么：本题中存在负数，并且数组是非递减顺序，
+ * 所以最大值只有可能出现在最左侧（负数数值越小，平方后越大）和最右侧（正数数值越大，平方后越大）
+ * 并且平方值向数组中间依次减小
+ * 怎么做：考虑双指针异地处理，比较左指针和右指针对应的值的平方，大的就尾插到异地的数组中
+ * 为了避免使用insert函数产生额外的时间复杂度，可以考虑提前开辟空间，并使用一个指针，起始位置指向异地数组的最后一个元素的位置
+ */
+// 左闭右闭区间
+class Solution977_2
+{
+public:
+    std::vector<int> sortedSquares(std::vector<int> &nums)
+    {
+        int left = 0, right = nums.size() - 1;
+        std::vector<int> ret(nums.size());
+        int reti = ret.size() - 1;
+        while (left <= right)
+        {
+            int num1 = nums[left] * nums[left];
+            int num2 = nums[right] * nums[right];
+            if (num1 < num2)
+            {
+                ret[reti--] = num2;
+                right--;
+            }
+            else
+            {
+                ret[reti--] = num1;
+                left++;
+            }
+        }
+
+        return ret;
+    }
+};
+
+// 左闭右开区间
+class Solution977_3
+{
+public:
+    std::vector<int> sortedSquares(std::vector<int> &nums)
+    {
+        int left = 0, right = nums.size();
+        std::vector<int> ret(nums.size());
+        int reti = ret.size() - 1;
+        while (left < right)
+        {
+            int num1 = nums[left] * nums[left];
+            int num2 = nums[right - 1] * nums[right - 1];
+            if (num1 < num2)
+            {
+                ret[reti--] = num2;
+                right--;
+            }
+            else
+            {
+                ret[reti--] = num1;
+                left++;
+            }
+        }
+
         return ret;
     }
 };
