065f473aaf31eef57be0f4aa5c575fc4c2a2cf3d
H0308 <1848312235@qq.com>
2024-10-30 21:43:27 +0800

    模拟算法

--- Simulation/Simulation.cpp
+++ Simulation/Simulation.cpp
@@ -5,7 +5,8 @@
 #include <vector>
 using namespace std;
 
-class Solution
+// 力扣59.螺旋矩阵 II
+class Solution59
 {
 public:
     vector<vector<int>> generateMatrix(int n)
@@ -69,6 +70,69 @@ public:
         // 如果n为奇数时填充中间的数值
         if (n % 2)
             ret[startx][starty] = count;
+
+        return ret;
+    }
+};
+
+// 力扣54.螺旋矩阵
+class Solution54
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+        // 处理结果
+        vector<int> ret;
+        // 获取行和列
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // 定义上下左右边
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = m - 1;
+
+        while (top <= bottom && left <= right)
+        {
+            // 处理第一行
+            for (int i = left; i <= right; i++)
+            {
+                ret.push_back(matrix[left][i]);
+            }
+            // 第一行处理完毕
+            top++;
+
+            // 处理第一列
+            for (int i = top; i <= bottom; i++)
+            {
+                ret.push_back(matrix[i][right]);
+            }
+            // 第一列处理完毕
+            right--;
+
+            // 处理第二行
+            // 需要判断是否还有第二行
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                {
+                    ret.push_back(matrix[bottom][i]);
+                }
+                // 第二行处理完毕
+                bottom--;
+            }
+
+            // 处理第二列
+            // 需要判断是否还有第二列
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                {
+                    ret.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
 
         return ret;
     }
