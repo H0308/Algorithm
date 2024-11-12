a73ee8961eef2270342f437859ed73b33c3d7c7a
H0308 <1848312235@qq.com>
2024-11-12 17:49:34 +0800

    链表——反转链表

--- LinkedList/reverseList.cpp
+++ LinkedList/reverseList.cpp
@@ -27,7 +27,8 @@ struct ListNode
 
 // 力扣206.反转链表
 // 迭代：原地处理
-class Solution206_1
+// 写法1
+class Solution206_1_1
 {
 public:
     ListNode *reverseList(ListNode *head)
@@ -51,6 +52,33 @@ public:
                 prev = cur;
                 cur = temp;
             }
+        }
+
+        head = prev;
+
+        return head;
+    }
+};
+
+// 写法2
+class Solution206_1_2
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr)
+            return nullptr;
+        // 直接将prev初始化为nullptr
+        // 使cur每一次更改next时指向prev即可
+        ListNode *prev = nullptr;
+        ListNode *cur = head;
+
+        while (cur)
+        {
+            ListNode *temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
         }
 
         head = prev;
@@ -88,5 +116,31 @@ public:
         }
 
         return copyHead->next;
+    }
+};
+
+// 递归版本——双指针变型
+class Solution206_3
+{
+public:
+    ListNode *_reverseList(ListNode *cur, ListNode *prev)
+    {
+        // 递归终止条件——循环终止条件
+        if (cur == nullptr)
+            // 循环结束后的步骤
+            return prev;
+
+        // 反转——循环中的过程
+        ListNode *temp = cur->next;
+        cur->next = prev;
+        // 再次的赋值操作相当于为下一次的循环做准备
+        // 在递归中相当于再次调用翻转函数
+        return _reverseList(temp, cur);
+    }
+
+    ListNode *reverseList(ListNode *head)
+    {
+        // 相当于双指针中创建cur和prev并初始化
+        return _reverseList(head, nullptr);
     }
 };
