b27e490b16add4ca1dcc6d6edb458f9135f1fc92
EPSDA <1848312235@qq.com>
2024-11-22 11:47:25 +0800

    链表——重复的子字符串

--- String/repeatedSubstringPattern.cpp
+++ String/repeatedSubstringPattern.cpp
@@ -7,9 +7,43 @@ using namespace std;
 
 // 力扣459.重复的子字符串
 // 解法1：暴力解法
+class Solution459_1
+{
+public:
+    bool repeatedSubstringPattern(string s)
+    {
+        // 枚举子字符串的末尾位置
+        // 子串至少重复一次，所以只有一个字符时一定结果为false
+        for(int i = 1; i * 2 <= s.size(); i++)
+        {
+            // 子字符串重复多次，其一定是原字符串长度的倍数
+            if(s.size() % i == 0)
+            {
+                bool flag = true;
+                // 匹配当前子串是否是原字符串的基础子串
+                for(int j = i; j < s.size(); j++)
+                {
+                    // 不需要重新枚举子串
+                    if(s[j] != s[j - i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
 
+                if(flag)
+                {
+                    return true;
+                }
+            }
+        }
 
-// 解法2：移动匹配
+        // 循环走完说明原字符串并不是由某一个子串重复多次构成
+        return false;
+    }
+};
+
+// 解法2：双倍字符串
 class Solution459_2
 {
 public:
