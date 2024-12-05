78d22f926c2b03a6c9a1a416ff29e35f1a2ec187
H0308 <1848312235@qq.com>
2024-12-05 10:19:09 +0800

    栈和队列——用队列实现栈

--- StackAndQeueue/MyStack.cpp
+++ StackAndQeueue/MyStack.cpp
@@ -6,62 +6,70 @@
 using namespace std;
 
 // 力扣225.用队列实现栈
-class MyStack {
+class MyStack
+{
 private:
     queue<int> q1;
     queue<int> q2;
-public:
-    MyStack() {
 
+public:
+    MyStack()
+    {
     }
 
-    void push(int x) {
+    void push(int x)
+    {
         // 插入到不为空的队列末尾
-        if(!q1.empty())
+        if (!q1.empty())
             q1.push(x);
         else
             q2.push(x);
     }
 
-    int pop() {
+    int pop()
+    {
         int back = 0;
         // 找到非空队列的最后一个
-        while(!q1.empty())
+        while (!q1.empty())
         {
-            back = q1.back();
-            if(q1.size() == 1)
+            back = q1.front();
+            if (q1.size() == 1)
             {
                 q1.pop();
                 return back;
             }
+            q2.push(back);
             q1.pop();
         }
 
-        while(!q2.empty())
+        while (!q2.empty())
         {
-            back = q2.back();
-            if(q2.size() == 1)
+            back = q2.front();
+            if (q2.size() == 1)
             {
                 q2.pop();
                 break;
             }
+            q1.push(back);
             q2.pop();
         }
 
         return back;
     }
 
-    int top() {
+    int top()
+    {
         // 找到非空队列的对头元素
-        if(!q1.empty())
+        if (!q1.empty())
         {
-            return q1.front();
+            return q1.back();
         }
 
-        return q2.front();
+        return q2.back();
     }
 
-    bool empty() {
+    bool empty()
+    {
         return q1.empty() && q2.empty();
     }
 };
