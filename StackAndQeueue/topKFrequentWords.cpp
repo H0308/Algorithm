e7c070ee99b0d5a88c67cd39e7c20a33c8b2fad3
H0308 <1848312235@qq.com>
2024-12-08 15:25:49 +0800

    栈和队列——前K个高频单词

--- StackAndQeueue/topKFrequentWords.cpp
+++ StackAndQeueue/topKFrequentWords.cpp
@@ -12,31 +12,33 @@ using psi = pair<string, int>;
 
 struct Compare
 {
-    bool operator()(const psi& p1, const psi& p2)
+    bool operator()(const psi &p1, const psi &p2)
     {
         return p1.second > p2.second || p1.second == p2.second && p1.first < p2.first;
     }
 };
 
 // 力扣692.前K个高频单词
-class Solution {
+class Solution
+{
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
+    vector<string> topKFrequent(vector<string> &words, int k)
+    {
         priority_queue<psi, vector<psi>, Compare> que;
 
         // map统计单词的次数
         map<string, int> fre;
 
-        for(const auto& str : words)
+        for (const auto &str: words)
         {
             fre[str]++;
         }
 
         // 向队列中插入数据
-        for(const auto& it: fre)
+        for (const auto &it: fre)
         {
             que.push(it);
-            if(que.size() > k)
+            if (que.size() > k)
             {
                 que.pop();
             }
@@ -44,7 +46,7 @@ public:
 
         // 存储返回值
         vector<string> ret(k);
-        for(int i = ret.size() - 1; i >= 0; i--)
+        for (int i = ret.size() - 1; i >= 0; i--)
         {
             ret[i] = que.top().first;
             que.pop();
