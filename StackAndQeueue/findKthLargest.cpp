dbd89dd7d881b5352f33278bd9f072856b31321f
H0308 <1848312235@qq.com>
2024-12-08 15:25:57 +0800

    栈和队列——数组中的第K个最大元素

---
+++ StackAndQeueue/findKthLargest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by 18483 on 2024/12/8.
+//
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// 力扣215.数组中的第K个最大元素
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        // 建小堆
+        priority_queue<int, vector<int>, greater<int>> que(nums.begin(), nums.end());
+
+        // 弹出指定个数的元素
+        int i = nums.size() - k;
+        while (i--)
+        {
+            que.pop();
+        }
+
+        return que.top();
+    }
+};
