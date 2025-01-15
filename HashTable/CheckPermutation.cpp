d194f8eb5b60f524906c1614b38206e1349307ec
H0308 <1848312235@qq.com>
2025-01-15 19:09:20 +0800

    哈希表——面试题01.02.判定是否互为字符重排

---
+++ HashTable/CheckPermutation.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 18483 on 2025/1/15.
+//
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 力扣面试题01.02.判定是否互为字符重排
+class Solution0102
+{
+public:
+    bool CheckPermutation(string s1, string s2)
+    {
+        // 长度不等一定不是重排
+        if (s1.size() != s2.size())
+            return false;
+
+        int hash[26] = {0};
+        // for (auto& ch : s2)
+        //     hash[ch - 'a']++;
+        for_each(s2.begin(), s2.end(), [&](char ch)
+        {
+            hash[ch - 'a']++;
+        });
+
+        for (auto &ch: s1)
+            if (--hash[ch - 'a'] < 0)
+                return false;
+
+
+        return true;
+    }
+};
